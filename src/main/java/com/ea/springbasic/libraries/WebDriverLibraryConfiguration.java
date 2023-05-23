77450ad82ec48c873369a4a5a96b418e75defda9
Ivan Aksionau <ivanionov2015@mail.ru>
2023-05-23 12:55:34 +0300

    - update

--- src/main/java/com/ea/springbasic/libraries/WebDriverLibraryConfiguration.java
+++ src/main/java/com/ea/springbasic/libraries/WebDriverLibraryConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebDriverLibrary {
+public class WebDriverLibraryConfiguration {
 
     @Bean
     @ConditionalOnProperty(name = "browser", havingValue = "chrome")
