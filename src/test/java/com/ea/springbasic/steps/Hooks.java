72d92c1936aecb2f79fb66fcc1af700eae5b4a88
Ivan Aksionau <ivanionov2015@mail.ru>
2023-07-03 19:36:27 +0300

    - update

--- src/test/java/com/ea/springbasic/steps/Hooks.java
+++ src/test/java/com/ea/springbasic/steps/Hooks.java
@@ -7,6 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+/**
+ * This class should contain methods annotated with
+ * Cucumber's {@link Before} and {@link After} annotations.
+ * These methods will be executed before and after each scenario.
+ */
 public class Hooks {
 
     @Autowired
@@ -16,12 +21,12 @@ public class Hooks {
     private String appUrl;
 
     @Before
-    public void initializeTest(Scenario scenario) {
+    public void setup(Scenario scenario) {
         webDriver.navigate().to(appUrl);
     }
 
     @After
-    public void tearDownTest(Scenario scenario) {
+    public void teardown(Scenario scenario) {
         if (scenario.isFailed()) {
             //Take screenshot logic goes here
             System.out.println(scenario.getName());
